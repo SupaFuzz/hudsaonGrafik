b1e6de228564c3aee2a7611a72f3a5e00de485d0
Amy Hicox <amy@hicox.com>
2021-02-07 22:25:19 -0600

    yar

--- cgi-bin/inventoryUpdate.pl
+++ cgi-bin/inventoryUpdate.pl
@@ -10,6 +10,7 @@ my $q = CGI->new;
 
 print "Content-type: text/html\n\n";
 print "<h1>", time(), "</h1>\n";
+print "<h2>version: " . $ENV{MOD_PERL} . "</h2>\n";
 print "<div class='data'>\n";
 
 #my @names = $q->param;
@@ -26,6 +27,12 @@ open (IN, $q->upload('inventoryCSVFile'));
 my $g = join('', <IN>);
 close(IN);
 print $g . "\n";
+
+## none of this shit works. We're gonna have to go mod_perl and I'm not even feeling that.
+## at the same time ... god please no proxies to an express server. That's way too far!
+## this should work. It ran the entirety of the internet for like 20 years!
+## but still yeah mod_perl is probably where it's at
+
 #print $q->upload('inventoryCSVFile') . "\n";
 
 # ok y'know what, this is a pain in the arse
