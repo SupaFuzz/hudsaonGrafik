00b95bc98fd8b1ad875532374354aaffad4fdde3
Amy Hicox <amy@hicox.com>
2021-02-07 20:53:50 -0600

    firther testing file upload

--- cgi-bin/inventoryUpdate.pl
+++ cgi-bin/inventoryUpdate.pl
@@ -4,10 +4,19 @@ use strict;
 use warnings;
 use CGI;
 
-my $p = CGI->new;
-my $data = $p->param('POSTDATA');
+my $q = CGI->new;
+#my $data = $p->param('POSTDATA');
 
 
 print "Content-type: text/html\n\n";
 print "<h1>", time(), "</h1>\n";
-print "<h2>", $data, "</h2>\n";
+print "<div class='data'>\n";
+
+if ( my $io_handle = $q->upload('inventoryCSVCFile') ) {
+    my $buffer;
+    while ( my $bytesread = $io_handle->read($buffer,1024) ) {
+        print "<pre>" . $buffer . "</pre>";
+    }
+}
+
+print "</div>"
