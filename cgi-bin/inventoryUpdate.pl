a88bf1b77d8ab31838e8e1f533fc9f0a5b8af9b1
Amy Hicox <amy@hicox.com>
2021-02-15 17:08:42 -0600

    yar/

--- cgi-bin/inventoryUpdate.pl
+++ cgi-bin/inventoryUpdate.pl
@@ -2,10 +2,6 @@
 
 use strict;
 use warnings;
-use CGI;
-
-my $q = CGI->new;
-#my $data = $p->param('POSTDATA');
 
 
 print "Content-type: text/html\n\n";
@@ -13,43 +9,8 @@ print "<h1>", time(), "</h1>\n";
 print "<h2>version: " . $ENV{MOD_PERL} . "</h2>\n";
 print "<div class='data'>\n";
 
-#my @names = $q->param;
-#foreach my $name (@names){
-#    print "<strong>" . $name . ": </strong><span>" . $q->param($name) . "</span><br>\n";
-#}
-
-## this, unfortunately is merely the filename
-$q->param('inventoryCSVFile');
-
-## this does not work
-#print $q->upload('inventoryCSVFile') . "\n";
-open (IN, $q->upload('inventoryCSVFile'));
-my $g = join('', <IN>);
-close(IN);
-print $g . "\n";
-
-## none of this shit works. We're gonna have to go mod_perl and I'm not even feeling that.
-## at the same time ... god please no proxies to an express server. That's way too far!
-## this should work. It ran the entirety of the internet for like 20 years!
-## but still yeah mod_perl is probably where it's at
-
-#print $q->upload('inventoryCSVFile') . "\n";
-
-# ok y'know what, this is a pain in the arse
-# thinking I'm just gonna go full REST and take
-# a single JSON object
-# oh god. oh wait. this is perl. no json.
-
-# I really hate to have to install node and 6GB of node_modules for this
-# ... buhgoddayum ...
-# perl is great. CPAN sucks the donkeynut
-
-
-#if ( my $io_handle = $q->upload('inventoryCSVFile') ) {
-#    my $buffer;
-#    while ( my $bytesread = $io_handle->read($buffer,1024) ) {
-#        print "<pre>" . $buffer . "</pre>";
-#    }
-#}
+foreach my $d (@INC){
+  print "<h3>" . $d . "</h3>\n";
+}
 
 print "</div>"
