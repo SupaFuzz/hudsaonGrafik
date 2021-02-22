21b7d05c5317e1e1d91cdff712e70f5e0496a16e
Amy Hicox <amy@hicox.com>
2021-02-21 22:58:22 -0600

    hudsonGrafikAdmin.js LOH #50. First test run of tonic is amazingly syntax free mostly!

--- admin/lib/hudsonGrafikAdmin.js
+++ admin/lib/hudsonGrafikAdmin.js
@@ -8,6 +8,7 @@ constructor(args, defaults, callback){
     super(args, tcObjectCore.mergeClassDefaults({
         _version:             1,
         _className:           'hudsonGrafikAdmin',
+        _dataFiles:           {}
     }, defaults), callback);
 }
 
@@ -22,15 +23,135 @@ startup(){
         that.log(`startup initiated`);
 
         // fetch the csv files
-        
+        let pk = [];
+        ['consumables_inventory.csv', 'parts_inventory.csv', 'product_inventory.csv'].forEach(function(fileName){
+            console.log(`fetch ${fileName}`)
+            pk.push(
+                that.fetch({
+                    endpoint:           `../data/${fileName}`,
+                    method:             'GET',
+                    expectHtmlStatus:   200
+                }).catch(function(error){
+                    console.log(`fetch error: ${error}`)
+                    throw(error);
+                }).then(function(xhr){
+                    console.log('parse')
+                    // xhr.response has the data which we need to parse
+                    that._dataFiles[fileName] = that.CSVToArray(xhr.response);
+                })
+            );
+        });
+        Promise.all(pk).catch(function(error){
+            throw(error);
+        }).then(function(){
+            console.log('all files fetched: ', that._dataFiles);
 
+            /*
+                LOH 2/21/21 @ 2253
+                everything works up to here, next step is to render it all on the screen
+                in such a way as it's editable. Lots of cool ideas come to mind, but the
+                simplest brutalist is to make an "add a row" form at the bottom and a
+                "remove the row" checkbox on each row. Old school and straight forward.
 
+                an in-place edit widget would kick ass though
+                actually ... as would an adaptation of the formView idea.
+                each column being a field. Or perhaps selecting a row and having
+                a drop in replacement thing I dunno ... 
+            */
 
-        toot(that);
-
+            toot(that);
+        })
     }));
 }
 
+
+
+
+/*
+    CSVToArray( strData, strDelimiter)
+	https://www.bennadel.com/blog/1504-ask-ben-parsing-csv-strings-with-javascript-exec-regular-expression-command.htm
+*/
+ CSVToArray( strData, strDelimiter ){
+
+	// comma is default delimiter
+	strDelimiter = (strDelimiter || ",");
+
+	// Create a regular expression to parse the CSV values.
+	var objPattern = new RegExp(
+		(
+			// Delimiters.
+			"(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+			// Quoted fields.
+			"(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+			// Standard fields.
+			"([^\"\\" + strDelimiter + "\\r\\n]*))"
+		),
+		"gi"
+		);
+
+
+	// Create an array to hold our data. Give the array
+	// a default empty first row.
+	var arrData = [[]];
+
+	// Create an array to hold our individual pattern
+	// matching groups.
+	var arrMatches = null;
+
+
+	// Keep looping over the regular expression matches
+	// until we can no longer find a match.
+	while (arrMatches = objPattern.exec( strData )){
+
+		// Get the delimiter that was found.
+		var strMatchedDelimiter = arrMatches[ 1 ];
+
+		// Check to see if the given delimiter has a length
+		// (is not the start of string) and if it matches
+		// field delimiter. If id does not, then we know
+		// that this delimiter is a row delimiter.
+		if (
+			strMatchedDelimiter.length &&
+			(strMatchedDelimiter != strDelimiter)
+			){
+
+			// Since we have reached a new row of data,
+			// add an empty row to our data array.
+			arrData.push( [] );
+
+		}
+
+
+		// Now that we have our delimiter out of the way,
+		// var's check to see which kind of value we
+		// captured (quoted or unquoted).
+		if (arrMatches[ 2 ]){
+
+			// We found a quoted value. When we capture
+			// this value, unescape any double quotes.
+			var strMatchedValue = arrMatches[ 2 ].replace(
+				new RegExp( "\"\"", "g" ),
+				"\""
+				);
+
+		} else {
+
+			// We found a non-quoted value.
+			var strMatchedValue = arrMatches[ 3 ];
+
+		}
+
+
+		// Now that we have our value string, var's add
+		// it to the data array.
+		arrData[ arrData.length - 1 ].push( strMatchedValue );
+	}
+
+	// Return the parsed data.
+	return( arrData );
+} // end CSVToArray
 
 
 
